648987b30fbc5be2da6cc081e97cbe46fb2a6f3e
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-08 12:16:31 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Portfolio from './components/Portfolio'
 import { getPriceUSD } from './lib/api'
 import { connectWallet, getUserData, signOut, getUserAddressSafe, openTransfer } from './lib/wallet'
 
-export default fuction App(){
+export default uction App(){
   const [addrsses, setAddresse] = useState(()=> {
     try { return JSON.parse(localStorage.getItem('stx_addresses')||'[]') } catch(e){ return [] }
   })
