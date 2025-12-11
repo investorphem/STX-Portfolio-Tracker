e7f593038c69c62476cbd14b550b51b406c1fe73
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-11 19:41:02 +0100

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import { getAccountInfo, getTxsForAddress } fro
+import { getAccountInfo, getTxsForAddress } fr
 
 export default function Portfolio({addresses, removeAddress, price}){
   const [data, setData] = useState({})
