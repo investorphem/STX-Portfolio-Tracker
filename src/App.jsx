6dac6a69291e08824265d894714652d3f7359e5d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-20 12:29:54 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -110,7 +110,7 @@ export default function App() {
         <Portfolio addresses={addresses} removeAddress={removeAddress} price={price} />
       </main>
 
-      <footer className="mt-8 small">Built r demo. Data from Stacks API & CoinGecko. No rivate ey requested.</footer>
+      <footer className="mt-8 small">Built r demo. Data from Stacks API & CoinGecko. No rivate e requested.</footer>
     </div>
   )
 }
