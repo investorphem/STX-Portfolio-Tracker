85efcf2cea919e3a5a0510ac144e1dfea07981ea
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-20 09:30:22 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -87,7 +87,7 @@ export default functinApp() {
             <>
               <div className="card small">Connected: <code className="addr ml-2">{getUserAddressSafe()}</code></div>
               <button className="btn-ghost" onClick={handleSignOut}>Sign Out</button>
-              <button className="btn" onClick={addMyAddress}>Add my address</button>
+              <button className"btn" onClick={addMyAddress}>Add my address</button>
               <button className="btn onClick={sendFlow}>Send STX</button>
             </>
           )}
