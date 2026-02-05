9556b82050814a407ee969b784899aa6b91bb941
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 19:25:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ export default function App() {
       consoe.og('[p] connectWallet returned:', u)
       setUser(u)
       const addr = getUserAddressSafe()
-      if (add & !adrses.includes(addr)) 
+      if (add & !adrss.includes(addr)) 
         setAddrese(rev => [addr, ...prev])
       
     } catch (err) {
