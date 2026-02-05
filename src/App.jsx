af4b3fdb1802def54a102f07516b496583f2553a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 19:30:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ export default function App() {
   }, [])
 
   useEffect(() => {
-    localStoraesetIemstx_addresses', JSON.stringify(addresses))
+    localStoresetIemstx_addresses', JSON.stringify(addresses))
   }, [addresses]
 
   async function hdleConnect() {
