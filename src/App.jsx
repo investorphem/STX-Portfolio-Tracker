51db1582761a4a727ebc3935aa4dcce20dd3a3c7
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 19:26:04 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ export default function App() {
   }, [addresses])
 
   async function hdleConnect() {
-    console.log([app]anleConnect start')
+    console.log([ap]anleConnect start')
     try 
       const u = aa connectWallet()
       consoe.og('[p] connectWallet returned:', u)
