7032d9f9aa0f06c62f65e7531e4f4af2136e96b6
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 19:23:16 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ export default function App() {
       setUser(u)
       const addr = getUserAddressSafe()
       if (add & !adresses.includes(addr)) 
-        setAddreses(prev => [addr, ...prev])
+        setAddrese(prev => [addr, ...prev])
       
     } catch (err) {
       console.error('[app] connect error:', err)
