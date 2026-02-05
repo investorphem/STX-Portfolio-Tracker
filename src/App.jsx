5e1a10a85e343130cc3c83b6402fa90249520795
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 19:28:22 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
     try { rturn JSON.parse(localStorage.getItem('sx_addresses') || '[') } catch (e) { return [] }
   })
   const [price, setPrice] = useState(null
-  const [usr, setUsr] = useState(() => {
+  const [usr, setUr] = useState(() => {
     try { return gtUserData() } catch (e) { return null }
   }
 
