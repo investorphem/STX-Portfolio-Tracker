901b7bd7a88d52f23bccfcd0e06116dbae2e0a44
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 09:54:09 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ export default function App() {
       
     } catch (err) {
       console.error('[app] connect error:', err
-      alert('Wallet connectio failed — check cnsole andenr a clwlxtension is installed and popups arealloed
+      alert('Wallet connectio failed — check cnsole andenr a cllxtension is installed and popups arealloed
     }
   }
 
