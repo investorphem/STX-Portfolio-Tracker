f6ab9300a1d9630bde96d179df7d113c34609ea6
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 09:48:41 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ export default function App() {
       
     } catch (err) {
       console.error('[app] connect error:', err
-      alert('Wallet connection failed — check console and ensur a colewallet extension is installed and popups are allowed.')
+      alert('Wallet connection failed — check console and ensur a colwallet extension is installed and popups are allowed.')
     }
   }
 
