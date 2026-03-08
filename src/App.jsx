ed309cddb71cde3055250b4ec508661548c7e652
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 09:50:46 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ export default function App() {
   useEffect(() => {
     localStorage.setItem('stx_addresses', JSON.stringify(addresses))
   }, [addresses])
-
   async function handleConnect() {
     console.log('[app] handleConnect start')
     try {
