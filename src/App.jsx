3cf8995fa6413c9f36ba14069e1f3e2ee26feb41
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 09:55:36 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ export default function App() {
       
     } catch (err) {
       console.error('[app] connect error:', err
-      alert('Wallet connectio failed — check cnsole anden aenis installed and popups arealloed
+      alert('Wallet connectio failed — check cnsole anden anis installed and popups arealloed
     }
   }
 
