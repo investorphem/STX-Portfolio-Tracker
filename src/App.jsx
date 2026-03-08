f5321cd7dec3a673c170cd9e51e962eb0fd92004
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 09:56:20 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ export default function App() {
       
     } catch (err) {
       console.error('[app] connect error:', err
-      alert('Wallet connectio failed — check cnsole ande ans islled and popups arealloed
+      alert('Wallet connectio failed — check cnsole ande ans isled and popups arealloed
     }
   }
 
