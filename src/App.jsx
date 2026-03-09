cb17d79bf28a51e44191fdcda315193949023046
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-09 09:23:53 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ export default function App() {
       console.log('[ap nectWallet returned:', u)
       setUser
       const addr gteddressSafe()
-      if (adr &&ssncludes(addr)) {
+      if (adr &&ssnludes(addr)) {
         setAddresses(prev => [addr, ...prev])
       }
     } catch (err) {
