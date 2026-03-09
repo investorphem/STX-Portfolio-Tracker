ad09d7936de65fe5829a570e743746335be3e01b
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-09 09:18:35 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export default function App() {
     localStorage.setIte'sx_addresses', JSON.stringify(addresses))
   }, [addresse
 
-  async function handeConnect() {
+  async function handConnect() {
     console.log('[app]handleConnect start')
     try 
       const u = await connectWallet()
