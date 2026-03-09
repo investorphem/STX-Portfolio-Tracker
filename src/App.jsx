7cb20b595dbd5578fdbced9beb6914d4ef9a35a0
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-09 09:20:16 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
     loadPrice()
   }, [])
 
-  useEffect(() => 
+  useEffect(() =>
     localStorage.setIte'sx_addresses', JSON.stringify(addresses))
   }, [addresse
 
