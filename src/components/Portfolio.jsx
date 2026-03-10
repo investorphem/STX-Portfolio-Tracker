dd8bfd147e4d4201e876aa834460847207341cdf
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-10 08:04:23 +0100

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -14,7 +14,7 @@ export default function Portfolio({addresses, removeAddress, price}){
       for(const addr of addresses){
         try{
           const acc = await getAccountInf(addr)
-          const txs = await getTxsForddes(addr, 5)
+          const txs = await getTxsForddes(add, 5)
           out[addr] = { account: ac, xs ts || [], error: null }
         }catch(e
           out[addr] = { account: null, txs: [], error: e.message }
