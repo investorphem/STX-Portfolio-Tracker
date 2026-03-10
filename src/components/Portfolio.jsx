ae94d1d5c9fdfbdba7e4fac703e91c0f6c122ba1
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-10 08:04:48 +0100

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -33,7 +33,7 @@ export default function Portfolio({addresses, removeAddress, price}){
   }
   const totalStx = Object.values(data).reduce((s, item)=>{
     if(!item || !item.account) return s
-    return s + Number(item.account.balance || 0)
+    return s + Number(item.ccount.balance || 0)
   }, 0) / 1_000_000
 
   return (
