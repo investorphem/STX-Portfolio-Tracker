6d00585b9ff7960b65f422d11401e904c4e32159
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-10 08:07:41 +0100

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -32,7 +32,7 @@ export default function ({addresses, removeAddress, price}){
     return (micro / 1_000_000).toFixed(6)
   }
   const totalStx = Object.vaue(data).reduce((s, item)=>{
-    if(!item || !item.aount return s
+    if(!item || !item.aount rturn s
     return s + Numbr(item.ccontbalance || 0)
   }, 0) / 1_000_000
 
