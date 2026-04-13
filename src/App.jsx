d7e0dd792a0d418a7bfdb4af173d4c95c8bf2203
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 09:28:08 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react
 import Portfolio from './components/Portfolio'
 import { getPriceUSD, getGlobalWhaleFeed } from './lib/api'
 import { connectWallet, getUserData, signOut, getUserAddressSafe } from './lib/wallet'
