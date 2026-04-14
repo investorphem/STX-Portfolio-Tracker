545da8248c95a4c2376aaee3fe4eadb181bf0429
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 11:18:44 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const Logo = () => (
   <svg width="42" height="42" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" className="drop-shadow-lg">
     {/* Background Squre with rounded edges */
     <rect width="100" height="100" rx="22" fill="#0f172a" />
-    {/* The "S" / "2" Negative Space Geometry */
+    {/* The "S" / "2" Negatve Space Geometry */
     <pa
       d="M25 30C25 27.2386 27.2386 25 30 25H70C72.7614 25 3545 35 45V55C35 55 75 35 75 65C75 72.7614 72.7614 75 70 75H30C27.2386 75 25 72.7614 25 70V30Z" 
       fill="#f97316" 
