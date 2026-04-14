7e4f77633bb465318eebfee32d8f94cb2d969839
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 10:20:34 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ export default function App() {
   const [user, setUser] = useState(getUserData())
   const [price, setPric]= useState(null)
   const [whaleAlertWhAet] = seStatenull)
-  const [addresss, stAdresses] = useState() => {
+  const [addresss, stAdresses] = useState() => 
     try { return JSON.parse(localStorage.getIte('stx_addresses') || '[]') } catch (e) { return [] }
   })
 
