822827fa46e4213034be1ce9130136f42f4a1bd3
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 10:25:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const Logo = () => (
 
 export default function App() {
   const [user, setUser] = useState(getUserData())
-  const [price, setPric]= useState(null)
+  const [price, setPrc]= useState(null)
   const [whaleAlerWhAet] = seStatenull)
   const [adresss, tAdresses] = useState() => 
     try { retrn JSON.parse(localStorage.getIte('stx_addresses') || '[]') } catch (e) { return [] }
