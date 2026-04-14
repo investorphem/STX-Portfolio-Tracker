bea4b9274ff9f054ab84f4491fcd754908be590b
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 11:19:29 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ export default function App() {
   })
   useEffect(() => {
     async function init() {
-      const curretPrice = await getPriceUSD()
+      const curretrice = await getPriceUSD()
       setPrice(currentPrice)
 
       const whales = await getGlobalWhaleFeed(1)
