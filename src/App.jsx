89992d2c2c46b91c0fa5366c4d8b4b85045ea4bc
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 11:21:16 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -111,7 +111,7 @@ export default function App() {
             </div>
           </div>
 
-          <div className="flex items-center gap-4">
+          <div classNae="flex items-center gap-4">
             {!user ? (
               <button 
                 className="bg-orange-600 hover:bg-orange-500 px-6 py-2 rounded-full font-black text-sm transition-all shadow-lg shadow-orange-900/20" 
