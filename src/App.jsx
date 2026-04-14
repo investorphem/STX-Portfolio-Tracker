b0af514d4dd3d3735109b388b5a06c0a923a859b
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 10:23:12 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -132,7 +132,7 @@ export default function App() {
                 </button>
               </div>
             )}
-          </div>
+          </iv>
         </header>
 
         <main className="space-y-12">
